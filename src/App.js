42da0f0eabaf7a2dac6fd077d3af82bde85f8f3f
siddhant-sri <sidz.dec26@gmail.com>
2023-07-04 01:14:54 +0530

    Class based components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import NavBar from './Components/Navbar';
+import News from './Components/News';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
-export default App;
+
+export default class App extends Component {
+  pageSize = 5;
+  apiKey = process.env.REACT_APP_NEWS_API;
+
+  state = {
+    progress : 0
+  }
+  setProgress = (progress) => {
+    this.setState({progress : progress})
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavBar/>
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress={this.state.progress}
+          />
+
+          <Routes>
+              <Route exact path='/' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="general" country = "in" category = "general"/>}></Route>
+              <Route exact path='/business' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="business" country = "in" category = "business"/>}></Route>
+              <Route exact path='/entertainment'  element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="entertainment" country = "in" category = "entertainment"/>}></Route>
+              <Route exact path='/general' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="general" country = "in" category = "general"/>}></Route>
+              <Route exact path='/health' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="health"
+               country = "in" category = "health"/>}></Route>
+              <Route exact path='/science' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="science" country = "in" category = "science"/>}></Route>
+              <Route exact path='/sports' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="sports" country = "in" category = "sports"/>}></Route>
+              <Route exact path='/technology' element={<News setProgress = {this.setProgress} apiKey={this.apiKey} pageSize = {this.pageSize} key="technology" country = "in" category = "technology"/>}></Route>
+          </Routes>
+        </Router> 
+      </div>
+    )
+  }
+}
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imgUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div>
+        <div className="card m-2">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger"> {source} </span>
+          </div>
+
+          <img
+            src={
+              imgUrl
+                ? imgUrl
+                : "http://cdn.wionews.com/sites/default/files/2023/05/29/355278-fwwvzkdwiaavg0m.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {author ? author : "Unknown"} at{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-dark btn-small"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
